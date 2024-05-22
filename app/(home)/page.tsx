8a7702eb47645c5b0a44122d10c5026b25a26442
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-05-22 18:33:23 +0530

    ➕added-the-landing-page-text-and-buttons🖲️

---
+++ app/(home)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <Button>Log in</Button>
+      <Button>Sign up</Button>
+    </div>
+  );
+}
