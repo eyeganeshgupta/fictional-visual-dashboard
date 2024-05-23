f3c5b336906b12b6cb6e02b262ebe6f119dc1f7b
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-05-23 22:34:20 +0530

    🧹tidying-up-the-landing-page-styles-and-link-to-login-and-signup-pages🌐

---
+++ app/(home)/layout.tsx
@@ -0,0 +1,11 @@
+type Props = {
+  children?: React.ReactNode;
+};
+
+export default function HomeLayout({ children }: Props) {
+  return (
+    <div className="flex flex-col gap-4 min-h-screen items-center justify-center p-24">
+      {children}
+    </div>
+  );
+}
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,24 @@
 import { Button } from "@/components/ui/button";
+import { PersonStandingIcon } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <Button>Log in</Button>
-      <Button>Sign up</Button>
-    </div>
+    <>
+      <h1 className="flex gap-2 items-center">
+        <PersonStandingIcon size={50} className="text-pink-500" />
+        Dashboard
+      </h1>
+      <p>The ultimate dashboard for customer support management.</p>
+      <div className="flex gap-2 items-center">
+        <Button asChild>
+          <Link href="/login">Log in</Link>
+        </Button>
+        <small>or</small>
+        <Button variant="outline" asChild>
+          <Link href="/sign-up">Sign up</Link>
+        </Button>
+      </div>
+    </>
   );
 }
