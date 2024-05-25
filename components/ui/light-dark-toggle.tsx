c8fda1f18888064ec1b0f9a8638a62dd630a00bd
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-05-25 19:10:01 +0530

    🌞light-and-🌙dark-mode-toggle

---
+++ components/ui/light-dark-toggle.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { MoonIcon, SunIcon } from "lucide-react";
+import { useState } from "react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./tooltip";
+
+type Props = {
+  className?: string;
+};
+
+export function LightDarkToggle({ className }: Props) {
+  const [isDarkMode, setIsDarkMode] = useState(true);
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger
+          className={className}
+          onClick={() => {
+            setIsDarkMode((preValue) => !preValue);
+            document.body.classList.toggle("dark");
+          }}
+        >
+          {isDarkMode ? <MoonIcon /> : <SunIcon />}
+        </TooltipTrigger>
+        <TooltipContent>
+          {isDarkMode ? "Enable light mode" : "Enable dark mode"}
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+}
