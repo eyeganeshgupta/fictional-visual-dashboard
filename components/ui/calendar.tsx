a66180a1d02361474c79034533bf9a0a3124d0f8
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-05-31 08:58:15 +0530

    ⚓hook-up-month-and-year-navigation-anytime-the-custom-dropdown-values-change🔄

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -60,7 +60,7 @@ function Calendar({
         IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
         IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
         Dropdown: (dropdownProps) => {
-          const { currentMonth } = useNavigation();
+          const { currentMonth, goToMonth } = useNavigation();
 
           const { fromYear, fromMonth, fromDate, toYear, toMonth, toDate } =
             useDayPicker();
@@ -100,7 +100,20 @@ function Calendar({
           );
 
           return (
-            <Select>
+            <Select
+              onValueChange={(newValue) => {
+                if (dropdownProps.name === "months") {
+                  const newDate = new Date(currentMonth);
+                  newDate.setMonth(parseInt(newValue));
+                  goToMonth(newDate);
+                } else if (dropdownProps.name === "years") {
+                  const newDate = new Date(currentMonth);
+                  newDate.setFullYear(parseInt(newValue));
+                  goToMonth(newDate);
+                }
+              }}
+              value={dropdownProps.value?.toString()}
+            >
               <SelectTrigger>{caption}</SelectTrigger>
               <SelectContent>
                 {selectValues.map((selectValue) => (
