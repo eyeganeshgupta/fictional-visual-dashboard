16a7f6e5c4d8ed29fbbb8c935d869d50b0cb46e8
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-06-04 19:58:08 +0530

    ☰ menu-title

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,3 +1,5 @@
+import MainMenu from "./components/main-menu";
+
 export default function DashboardLayout({
   children,
 }: {
@@ -5,7 +7,7 @@ export default function DashboardLayout({
 }) {
   return (
     <div className="grid grid-cols-[250px_1fr] h-screen">
-      <div className="bg-muted overflow-auto p-4">Side Panel</div>
+      <MainMenu />
       <div className="overflow-auto py-2 px-4">
         <h1 className="pb-4">Welcome back, Ganesh!</h1>
         {children}
