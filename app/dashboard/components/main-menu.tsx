2d65944c4f72fac083a3ae26f41c5c119a794bdd
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-06-05 19:54:59 +0530

    sidebar-footer-with-avatar🤵

--- app/dashboard/components/main-menu.tsx
+++ app/dashboard/components/main-menu.tsx
@@ -1,18 +1,32 @@
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { LightDarkToggle } from "@/components/ui/light-dark-toggle";
+import Link from "next/link";
 import MenuItem from "./menu-item";
 import MenuTitle from "./menu-title";
 
 export default function MainMenu() {
   return (
-    <div className="bg-muted overflow-auto p-4">
+    <div className="bg-muted overflow-auto p-4 flex flex-col">
       <div className="border-b dark:border-b-black border-b-zinc-300 pb-4">
         <MenuTitle />
       </div>
-      <div className="py-4">
+      <div className="py-4 grow">
         <MenuItem href="/dashboard">My Dashboard</MenuItem>
         <MenuItem href="/dashboard/teams">Teams</MenuItem>
         <MenuItem href="/dashboard/employees">Employees</MenuItem>
         <MenuItem href="/dashboard/account">Account</MenuItem>
         <MenuItem href="/dashboard/settings">Settings</MenuItem>
+      </div>
+      <div className="flex gap-2 items-center">
+        <Avatar>
+          <AvatarFallback className="bg-pink-300 dark:bg-pink-800">
+            GG
+          </AvatarFallback>
+        </Avatar>
+        <Link href="/" className="hover:underline">
+          Logout
+        </Link>
+        <LightDarkToggle className="ml-auto" />
       </div>
     </div>
   );
