a4d4defae6c7b6fb17df3e4a46dc47e8dab379e8
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-06-06 20:25:01 +0530

    ➕added-the-employees-stats-and-teams-stats📊

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,14 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 export default function DashboardPage() {
-  return <div>Dashboard Page</div>;
+  return (
+    <Tabs defaultValue="employees">
+      <TabsList>
+        <TabsTrigger value="employees">Employees Stats</TabsTrigger>
+        <TabsTrigger value="teams">Teams Stats</TabsTrigger>
+      </TabsList>
+      <TabsContent value="employees">Employees Stats View</TabsContent>
+      <TabsContent value="teams">Teams Stats View</TabsContent>
+    </Tabs>
+  );
 }
