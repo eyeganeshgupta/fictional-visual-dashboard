122d9f9776305fb9f7b37487e51df267e41c33a3
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-08-12 22:44:49 +0530

    menu-drawer

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,3 +1,8 @@
+"use client";
+
+import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer";
+import { useMediaQuery } from "@/hooks/use-media-query";
+import { MenuIcon } from "lucide-react";
 import MainMenu from "./components/main-menu";
 import MenuTitle from "./components/menu-title";
 
@@ -6,12 +11,25 @@ export default function DashboardLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const isDesktop = useMediaQuery("(min-width: 768px)");
+
   return (
     <div className="grid md:grid-cols-[250px_1fr] h-screen">
       <MainMenu className="hidden md:flex" />
-      <div className="p-4 block md:hidden sticky top-0 left-0 bg-background border-b border-border">
-        <MenuTitle />
-      </div>
+      {!isDesktop && (
+        <div className="p-4 flex justify-between md:hidden sticky top-0 left-0 bg-background border-b border-border">
+          <MenuTitle />
+          <Drawer>
+            <DrawerTrigger>
+              <MenuIcon />
+            </DrawerTrigger>
+            <DrawerContent>
+              <MainMenu />
+            </DrawerContent>
+          </Drawer>
+        </div>
+      )}
+
       <div className="overflow-auto py-2 px-4">
         <h1 className="pb-4">Welcome back, Ganesh!</h1>
         {children}
